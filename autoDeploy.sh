1128456bb7824a3ebf1abb4767e81f45ae3a4386
Wonseok Song <dnjstjr0517@gmail.com>
2020-10-07 04:19:05 +0900

    Fix: LOCAL과 REMOTE가 다른 문제 해결
    - REMOTE에 origin -> origin/master 로 변경

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -2,10 +2,10 @@ git fetch
 
 LOCAL=$(git rev-parse HEAD)
 
-REMOTE=$(git rev-parse origin)
+REMOTE=$(git rev-parse origin/master)
 
 if [ $LOCAL != $REMOTE ]; then
-	git pull origin master
-	npm i
-	pm2 reload app
+      git pull origin master
+      npm i
+      pm2 reload app
 fi
