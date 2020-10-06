91495fb26729b8784cfc35fa30b466c3939d279c
Wonseok Song <dnjstjr0517@gmail.com>
2020-10-07 04:04:25 +0900

    Update: autoDeploy.sh

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -1,15 +1,11 @@
 # 브랜치 이름을 변수에 할당
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+LOCAL=$(git rev-parse HEAD)
 
 # 현재 브랜치의 최종 커밋의 해시값
-HASH=$(git rev-parse HEAD)
+REMOTE=$(git rev-parse origin)
 
-# 원격에 업로드된 현재 브랜치의 커밋 해시
-REMOTE_HASH=$(git rev-parse --verify origin/$BRANCH)
-
-
-if [ $HASH != $REMOTE_HASH ]; then
-	git pull
+if [ $LOCAL != $REMOTE ]; then
+	git pull origin master
 	npm i
 	pm2 reload app
-fi 
+fi
