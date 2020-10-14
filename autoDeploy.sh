46005da8de95e8bc8c8632b1d02ddd3c086c9848
Wonseok Song <dnjstjr0517@gmail.com>
2020-10-14 23:46:22 +0900

    Refactor: 쉘 스크립트 웹팩 빌드 추가
    - 웹팩 빌드를 위해 내용 추가

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -9,5 +9,6 @@ REMOTE=$(git rev-parse origin/master)
 if [ $LOCAL != $REMOTE ]; then
       git pull origin master
       /home/wonseok/.nvm/versions/node/v14.13.0/bin/npm install
+      /home/wonseok/.nvm/versions/node/v14.13.0/bin/npx webpack
       /home/wonseok/.nvm/versions/node/v14.13.0/bin/pm2 reload app
 fi
